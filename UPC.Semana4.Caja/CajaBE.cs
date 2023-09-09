fe52ecb1c6a8a13a723db948bdd09e0eb8c43316
CHRISTIAN GENARO ROJAS RAMOS <PCISCROJ@upc.edu.pe>
2023-09-09 09:53:00 -0700

    Creación de entidades Caja y Compra

---
+++ UPC.Semana4.Caja/CajaBE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPC.Semana4.Caja
+{
+    public class CajaBE
+    {
+        public string id_caja { get; set; }
+        public string nombre_cajero { get; set; }
+    }
+}
---
+++ UPC.Semana4.Caja/CompraBE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPC.Semana4.Caja
+{
+    public class CompraBE
+    {
+        public string nro_documento { get; set; }
+        public decimal monto_compra { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
